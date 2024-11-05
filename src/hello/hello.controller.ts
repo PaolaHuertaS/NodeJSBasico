0cd44bc59cc2eeddf9a71d1041811ea892593a7e
nicol <nicolehs2015@hotmail.com>
2024-11-04 19:43:12 -0600

    http errores, pipes, terminar validaciones y guards

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, ParseBoolPipe, ParseIntPipe, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { ValidateuserPipe } from './pipes/validateuser/validateuser.pipe';
+import { AuthGuard } from './guards/auth/auth.guard';
 
 @Controller()
 export class HelloController {
@@ -9,6 +11,46 @@ export class HelloController {
        console.log(request.url)
         response.status(200).json({
             message: "Hola",
-        })
+        });
     }
+
+    @Get('new')
+    @HttpCode(201)
+    somethingNew(){
+        return "Something new";
+    }
+
+    @Get('notFound')
+    @HttpCode(404)
+    notFoundPage() {
+        return '404 not found';
+    }
+
+    @Get('error')
+    @HttpCode(500)
+    errorPage() {
+        return 'Error route';
+    }
+
+    @Get('ticket/:num')
+    getNumber(@Param('num', ParseIntPipe) num: number){
+        return num + 14 ;
+    }
+
+    @Get('active/:status')
+    @UseGuards(AuthGuard)
+    isUserActive(@Param('status', ParseBoolPipe) status: boolean){
+        console.log(typeof status)
+        return status;
+    }
+
+    @Get('greet')
+    @UseGuards(AuthGuard)
+    greet(@Query(ValidateuserPipe) query: {name: string, age: number}){
+        console.log(typeof query.name)
+        console.log(typeof query.age)
+        return 'Hello ${query.name}, you are ${query.age} years old' ;
+    }
+
+
 }
