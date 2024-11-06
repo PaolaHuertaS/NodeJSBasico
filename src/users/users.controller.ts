283bfe003b46191bc41631cd6b58c12e1a7d8700
nicol <nicolehs2015@hotmail.com>
2024-11-05 19:25:08 -0600

    Ultimo commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/createuser.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class UsersController {
    
-    constructor (private usersService: UsersService){}
+  constructor (private usersService: UsersService){}
    
+  @ApiTags("users")
    @Get('/users')
    getUsers(){
     return this.usersService.getUsers();
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import { AuthMiddleware } from './users/auth/auth.middleware';
+import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
+  imports: [PaymentsModule]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: '/users', method: RequestMethod.GET
+    }, {
+      path: '/users',
+      method: RequestMethod.POST
+    }).apply(AuthMiddleware)
+      .forRoutes('users');
+  }
+}
